32dffd5772cff03132825acfab5d5cdd73247ccf
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-03 21:13:36 -0600

    Explainer script (NON-FUNCTIONAL)

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import thermal
 import error_signaling
 
+def placeholder_database_and_rfid_stuff() -> int:
+    return 2
+
 def main() -> None:
     while True:
-        print(thermal.get_frame_data())
+        people_inside_range, people_outside_range = thermal.get_frame_data()
+
+        if people_inside_range != placeholder_database_and_rfid_stuff():
+            error_signaling.errorFlash()
 
 if '__name__' == '__main__':
     main()
