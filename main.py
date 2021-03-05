7d0d13d971e48bc0ad119a2a9e2995a101e360a5
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-04 23:19:54 -0600

    Placeholder code

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def placeholder_database_check(tags) -> int:
     return 0
 
 def main() -> None:
-
     tags = []
 
     input("Press enter to start")
 
+    print("Start of main loop")
     while True:
-        people_inside_range, people_outside_range = thermal.get_frame_data()
+        print("Getting Thermal data")
+        #people_inside_range, people_outside_range = thermal.get_frame_data()
+        people_inside_range = 2
+        people_outside_range = 2
 
         print(f"People inside range: {people_inside_range}\nPeople outside range: {people_outside_range}")
 
@@ -20,9 +23,14 @@ def main() -> None:
             print("People detected")
             tags = rfid_reader.call_reader()
             print(f"Tags detected: {tags}")
-            if len(tags) != people_inside_range or not placeholder_database_check(tags):
-                print("Error Detected!")
+            #if len(tags) != people_inside_range or not placeholder_database_check(tags):
+            if len(tags) != people_inside_range
+                print("Error Detected, tags != people inside range!")
                 error_signaling.errorFlash()
+            if placeholder_database_check(tags):
+                print("Error Detected, tags invalid or used!")
+                error_signaling.errorFlash()
+
             print("Successful loop")
 
 if '__name__' == '__main__':
