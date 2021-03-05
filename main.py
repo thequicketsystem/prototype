489c8c09d1035410521d63df01b538dcb0c02019
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-04 22:58:54 -0600

    Added Drew's RFID reader stuff

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import thermal
 import error_signaling
+import rfid_reader
 
-def placeholder_database_and_rfid_stuff() -> int:
+def placeholder_database_check() -> int:
     return 2
 
 def main() -> None:
+
+    tags = []
 
     input("Press enter to start")
 
     while True:
         people_inside_range, people_outside_range = thermal.get_frame_data()
 
-        if people_inside_range != placeholder_database_and_rfid_stuff():
-            error_signaling.errorFlash()
+        if people_inside_range > 0:
+            tags = rfid_reader.call_reader()
+            if len(tags) != people_inside_range or not placeholder_database_check(tags):
+                error_signaling.errorFlash()
 
 if '__name__' == '__main__':
     main()
