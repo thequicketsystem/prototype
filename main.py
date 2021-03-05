0a0060f75bf2872e0eff3962248d371ec152bd39
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-04 23:06:14 -0600

    Added some print statements for output

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import thermal
 import error_signaling
 import rfid_reader
 
-def placeholder_database_check() -> int:
-    return 2
+def placeholder_database_check(tags) -> int:
+    return 0
 
 def main() -> None:
 
@@ -14,10 +14,16 @@ def main() -> None:
     while True:
         people_inside_range, people_outside_range = thermal.get_frame_data()
 
+        print(f"People inside range: {people_inside_range}\nPeople outside range: {people_outside_range}")
+
         if people_inside_range > 0:
+            print("People detected")
             tags = rfid_reader.call_reader()
+            print(f"Tags detected: {tags}")
             if len(tags) != people_inside_range or not placeholder_database_check(tags):
+                print("Error Detected!")
                 error_signaling.errorFlash()
+            print("Successful loop")
 
 if '__name__' == '__main__':
     main()
