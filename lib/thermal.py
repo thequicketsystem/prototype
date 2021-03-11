30de976efc5c5d7bd3cc45c36273069d463bf8d0
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-10 22:51:18 -0600

    Incorporated most recent changes to thermal

--- lib/thermal.py
+++ lib/thermal.py
@@ -25,52 +25,22 @@ mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 f = [0] * (IMG_WIDTH * IMG_HEIGHT)
 
-# Set up detector parameters
-params = cv2.SimpleBlobDetector_Params()
-
-# Change thresholds
-params.minThreshold = 0;
-params.maxThreshold = 255;
-
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 600
-params.maxArea = 4000
-
-# Filter by Circularity
-params.filterByCircularity = True
-params.minCircularity = 0.1
-
-# Filter by Convexity
-params.filterByConvexity = False
-params.minConvexity = 0.01
-
-# Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.1
-
-
-def get_frame_data() -> tuple:
-    """ 
-    Returns a tuple (int, int) consisting of the number of people inside the range
-    of the rfid reader and the number of people outside the range of the rfid reader.
-    """
-    temp_data = np.empty([IMG_WIDTH, IMG_HEIGHT])
+while True:
+    #temp_data = np.empty([IMG_WIDTH, IMG_HEIGHT])
 
     try:
         mlx.getFrame(f)
     except ValueError:
-        pass
+        continue
 
-    #TODO: Test the numpy-reshape branch solution here (ONLY AFTER EVERYTHING ELSE WORKS!!)
-    for y in range(IMG_HEIGHT):
-        for x in range(IMG_WIDTH):
-            temp_data[x, y] = f[y * IMG_WIDTH + x]  
+    #for y in range(IMG_HEIGHT):
+    #    for x in range(IMG_WIDTH):
+    #        temp_data[x, y] = f[y * IMG_WIDTH + x]
+    
+    temp_data = np.array(f).reshape((IMG_HEIGHT, IMG_WIDTH))
 
     temp_data = cv2.resize(temp_data, dsize=(IMG_WIDTH * SCALE_FACTOR, IMG_HEIGHT * SCALE_FACTOR))
     temp_data = cv2.normalize(temp_data, temp_data, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-
-    colorized_temp_data = cv2.applyColorMap(temp_data, cv2.COLORMAP_JET)
 
     temp_data = cv2.bilateralFilter(temp_data, 9, 150, 150)
 
@@ -83,20 +53,25 @@ def get_frame_data() -> tuple:
 
     temp_data = cv2.bitwise_not(temp_data)
 
-    #TODO: Explore whether moving this outside function call is possible/performance improvement
-    detector = cv2.SimpleBlobDetector_create(params)    
+    # Change thresholds
+    params.minThreshold = 0;
+    params.maxThreshold = 255;
+
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 750
+    params.maxArea = 8000
+
+    # Filter by Circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0.1
+    
+    # Filter by Inertia
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.01
+    
+    detector = cv2.SimpleBlobDetector_create(params)
 
     keypoints = detector.detect(temp_data)
 
-    # Determine which blobs are inside the circle and which are outside
-    insideReaderRange = 0
-    outsideReaderRange = 0
-
-    for kp in keypoints:
-        d = sqrt(pow(kp.pt[0] - circleX, 2) + pow(kp.pt[1] - circleY, 2))
-        if d < circleR:
-            insideReaderRange += 1
-        else:
-            outsideReaderRange += 1
-    
-    return (insideReaderRange, outsideReaderRange)
+    return(len(keypoints))
