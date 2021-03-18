8ac4edf9c64707204a2466366b72985e814bba43
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-17 20:44:29 -0500

    First commit of new multiprocessing approach (EXPERIMENTAL, NON-FUNCTIONAL PROBABLY)

--- main.py
+++ main.py
@@ -5,35 +5,44 @@ import lib.database as database
 from time import sleep
 from concurrent.futures import ProcessPoolExecutor
 
+# Defines length of time to wait before checking
+# process status. Set to 5ms for now
+POLLING_DELAY = 0.005
+
 def get_best_of_five() -> int:
   return thermal.get_best_of_x(5)
 
 def main() -> None:
+    error_signaling.setGreen()
+    
+    people_count = 0
     tags = []
 
-    error_signaling.setGreen()
-
-    with ProcessPoolExecutor(max_workers=1) as executor:
-        frame_data_future = executor.submit(get_best_of_five)
-        
+    with ProcessPoolExecutor(max_workers=2) as executor:
         while True:
-            people_count = frame_data_future.result()
-            executor.submit(get_best_of_five)
-            print(f"People count: {people_count}")
+            # TODO: This is super messy and seriously needs to be redone but
+            # it should work for now. Circle back for improvement
+            # if it actually does work.
+            frame_data_future = executor.submit(get_best_of_five)
+            rfid_data_future = executor.submit(rfid_reader.call_reader)
 
-            if people_count > 0:
-                tags = rfid_reader.call_reader()
-                print(f"People detected, read tags: {tags}")
-                
-                if len(tags) != people_count:
-                    print(f"Error detected: Number of tags does not match number of people!")
-                    error_signaling.errorFlash()
-                
-                if any(x == False for x in database.readTicketList(tags)):
-                    print(f"Error detected: Invalid/Used tag!")
-                    error_signaling.errorFlash()
+            while True:
+                if rfid_data_future.done():
+                    tags = rfid_data_future.result()
+
+                    # Always check this first since this data is always available
+                    if any(x == False for x in database.readTicketList(tags)):
+                        print(f"Error detected: Invalid/Used tag!")
+                        error_signaling.errorFlash()
                     
+                    # Then check this
+                    elif len(tags) != frame_data_future.result():
+                        print(f"Error detected: Number of tags does not match number of people!")
+                        error_signaling.errorFlash()
+                    
+                    break
+
                 else:
-                    print("Successful loop!\n")
+                    sleep(POLLING_DELAY)
 
 main()
